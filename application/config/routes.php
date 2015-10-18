3245cdc4a5f11a36a4076709219d9a794bbf0a85
Kiang Kuang <kiangkuang@gmail.com>
2015-10-19 01:19:24 +0800

    reenable 404 error page

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'cca';
-$route['404_override'] = '';
+$route['404_override'] = 'Error';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['(?i)login'] = 'account/login';
