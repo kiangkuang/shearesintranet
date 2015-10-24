de6690d345324c6a1fa8f6d9bddcb3eb5bc63b8a
Kiang Kuang <kiangkuang@gmail.com>
2015-10-24 22:30:22 +0800

    versioning constant and footer

--- application/views/_layouts/footer.php
+++ application/views/_layouts/footer.php
@@ -2,7 +2,9 @@
       <footer class="main-footer">
         <!-- To the right -->
         <div class="pull-right hidden-xs">
-          <b>Version</b> 1.1.1
+          <b>Academic Year</b> <?= ACAD_YEAR ?>
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <b>Version</b> <?= VERSION ?>
         </div>
         <!-- Default to the left -->
         <strong>Copyright &copy; <?= '2015' !== date('Y') ? '2015 - ' . date('Y') : '2015'; ?> <a href="#">Kuek Kiang Kuang</a>.</strong> All rights reserved.
