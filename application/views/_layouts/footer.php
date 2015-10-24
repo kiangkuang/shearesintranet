4a95dcb04ec75cafbd1982489f37a92f63eb3b06
Kiang Kuang <kiangkuang@gmail.com>
2015-10-24 21:45:29 +0800

    auto copyright year

--- application/views/_layouts/footer.php
+++ application/views/_layouts/footer.php
@@ -5,5 +5,5 @@
           <b>Version</b> 1.1.1
         </div>
         <!-- Default to the left -->
-        <strong>Copyright &copy; 2015 <a href="#">Kuek Kiang Kuang</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; <?= '2015' !== date('Y') ? '2015 - ' . date('Y') : '2015'; ?> <a href="#">Kuek Kiang Kuang</a>.</strong> All rights reserved.
       </footer>
