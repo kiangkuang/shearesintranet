4aab867d7ce8119ff896ea0c84666248c35cc805
Kiang Kuang <kiangkuang@gmail.com>
2015-11-01 02:35:50 +0800

    cca type edit twig

--- application/controllers/Ccatype.php
+++ application/controllers/Ccatype.php
@@ -46,7 +46,8 @@ class Ccatype extends MY_Controller {
 
         $data['mainMenu'] = 'admin';
         $data['subMenu'] = 'cca';
-        $this->load->view('ccatype/edit', $data);
+        $data['this'] = $this;
+        $this->twig->display('ccatype/edit', $data);
     }
 
     public function update()
