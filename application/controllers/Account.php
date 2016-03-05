e1578e902a6f27061a6606f7a49681b9a3381842
Kiang Kuang <kiangkuang@gmail.com>
2016-03-05 22:21:22 +0800

    fix account import

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -423,6 +423,7 @@ class Account extends MY_Controller {
                 $csvFile = new Keboola\Csv\CsvFile($upload['full_path']);
                 $import = [];
                 $update = [];
+                $skipped = [];
                 $processedUsers = [];
                 foreach ($csvFile as $row) {
                     // ignore header row and empty names
@@ -439,7 +440,8 @@ class Account extends MY_Controller {
                         $importRow['acad_year'] = ACAD_YEAR;
 
                         if (array_search($importRow['user'], $processedUsers) !== false) {
-                            break;
+                            $skipped[] = ['row' => implode($row,', '), 'reason' => 'Repeated Account in this imported file'];
+                            continue;
                         }
 
                         $existingRow = $this->accounts_model->getByUserAcadYear($importRow['user'], ACAD_YEAR);
@@ -464,10 +466,13 @@ class Account extends MY_Controller {
                 if (isset($importResult) && $importResult !== false || isset($updateResult) && $updateResult !== false) {
                     $successMsg = count($import) ? count($import) . ' new accounts added!<br>' : '';
                     $successMsg .= count($update) ? count($update) . ' existing accounts updated!' : '';
+                    $warningMsg = count($skipped) ? count($skipped) . ' rows not imported!' : '';
 
                     $this->session->set_flashdata('success', $successMsg);
+                    $this->session->set_flashdata('warning', $warningMsg);
                     $this->session->set_flashdata('imported', $import);
                     $this->session->set_flashdata('updated', $update);
+                    $this->session->set_flashdata('skipped', $skipped);
                     redirect('account/import');
                 } else {
                     $this->session->set_flashdata('error', 'Nothing imported!');
@@ -483,6 +488,9 @@ class Account extends MY_Controller {
         }
         if ($this->session->updated) {
             $data['updated'] = $this->session->updated;
+        }
+        if ($this->session->skipped) {
+            $data['skipped'] = $this->session->skipped;
         }
 
         $data['lastAcadYear'] = substr(ACAD_YEAR, 0, 2)-1 . '/' . substr(ACAD_YEAR, 0, 2);
